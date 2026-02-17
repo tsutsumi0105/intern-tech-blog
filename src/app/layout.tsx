c599ba6ed2c0db3e644d56fbb979f0ccb2d4853a
堤健悟 <tsutsumikengo@tsutsumikengonoMac-mini.local>
2026-02-17 12:23:50 +0900

    feat:フッターが画面の一番下に固定されるように修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         className={`bg-[#F9FAFB] min-h-screen flex flex-col ${inter.className}`}
       >
         <Header />
-        <main>{children}</main>
+        <main className="flex-1">{children}</main>
         <Footer />
       </body>
     </html>
