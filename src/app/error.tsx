f373d5df8d4678779a0da294f2f52cb5aa232955
堤健悟 <tsutsumikengo@tsutsumikengonoMac-mini.local>
2026-02-19 13:08:20 +0900

    refactor:エラー文章を日本語に変更

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -15,16 +15,8 @@ export default function Error({
 
   return (
     <div>
-      <h2>Something went wrong!</h2>
+      <h2>エラーが発生しました</h2>
       <p>{error.message}</p>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
     </div>
   );
 }
