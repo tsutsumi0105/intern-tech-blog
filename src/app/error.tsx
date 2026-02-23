998e20867432644c9c0512bed23a72528f5ec57f
堤健悟 <tsutsumikengo@tsutsumikengonoMac-mini.local>
2026-02-23 17:49:38 +0900

    refactor: error.tsxの不要なコードを削除

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,18 +1,12 @@
 "use client";
-import { useEffect } from "react";
 import Image from "next/image";
 
 export default function Error({
-  error,
   reset,
 }: {
   error: Error & { digest?: string };
   reset: () => void;
 }) {
-  useEffect(() => {
-    console.error(error);
-  }, [error]);
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-6">
       <Image
