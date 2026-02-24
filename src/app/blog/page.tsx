c98d97704e7b6a16164d0f1d4014625964149ccd
堤健悟 <tsutsumikengo@tsutsumikengonoMac-mini.local>
2026-02-25 08:40:51 +0900

    refactor:記事一覧ページ内の表示を、コンポーネントを使用したものに変更

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,17 +1,10 @@
 export const dynamic = "force-dynamic";
 import { client } from "@/lib/microcms";
-import Link from "next/link";
-import Image from "next/image";
 import HomeIcon from "@/components/Icons/HomeIcon";
 import DocIcon from "@/components/Icons/DocIcon";
-import ArrowIcon from "@/components/Icons/ArrowIcon";
-import TagIcon from "@/components/Icons/TagIcon";
-import CalendarIcon from "@/components/Icons/CalendarIcon";
 import BreadCrumb, { BreadCrumbItem } from "@/components/BreadCrumb";
 import Pagination from "@/components/Pagination";
-import type { Blog } from "@/types/microcms";
-import { format } from "date-fns";
-import { ja } from "date-fns/locale";
+import ArticleCard from "@/components/ArticleCard";
 
 type Props = {
   searchParams?: Promise<{ page?: string }>;
@@ -69,58 +62,7 @@ export default async function BlogPage({ searchParams }: Props) {
         </div>
 
         {/*記事*/}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {blogs.map((blog: Blog) => (
-            <Link
-              key={blog.id}
-              href={`/blog/${blog.id}`}
-              className="bg-white rounded-xl shadow-md overflow-hidden hover:shadow-lg transition"
-            >
-              {/* 画像 */}
-              <div className="h-50 relative">
-                <Image
-                  src={blog.eyecatch.url}
-                  alt={blog.title}
-                  fill
-                  className="object-cover"
-                />
-                {/* タグ */}
-                <div className="absolute  px-3 py-1.5 top-4 right-4 inline-flex gap-1.5 border border-primary-soft  rounded-full w-fit shadow-md  bg-white/95  text-primary items-center">
-                  <TagIcon width={12} height={12} />
-                  {blog.tags.map((tag) => (
-                    <span
-                      key={tag.id}
-                      className="text-xs font-semibold px-2 py-1"
-                    >
-                      {tag.name}
-                    </span>
-                  ))}
-                </div>
-              </div>
-              {/* 本文 */}
-              <div className="flex flex-col p-6 gap-4">
-                <div className="flex gap-2 items-center">
-                  <CalendarIcon width={16} height={16} />
-                  <span className="text-text-tertiary text-sm mt-auto">
-                    {format(new Date(blog.publishedAt), "yyyy年M月d日", {
-                      locale: ja,
-                    })}
-                  </span>
-                </div>
-                <h3 className="font-semibold text-2xl text-text-main line-clamp-2">
-                  {blog.title}
-                </h3>
-                <p className="text-sm text-text-secondary">
-                  {blog.description}
-                </p>
-                <div className="flex gap-2  text-primary items-center">
-                  <span className="text-sm">記事を読む</span>
-                  <ArrowIcon width={16} height={16} />
-                </div>
-              </div>
-            </Link>
-          ))}
-        </div>
+        <ArticleCard blogs={blogs} />
         {/*ページネーション*/}
         <Pagination
           currentPage={page}
