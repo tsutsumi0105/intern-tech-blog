a7753789ae0ee4d5e10b62f00a03dfc5626b43a3
堤健悟 <tsutsumikengo@tsutsumikengonoMac-mini.local>
2026-02-24 17:27:01 +0900

    fix:記事にタグが設定されていない場合、タグが非表示になるよう変更。

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -29,17 +29,19 @@ export default function ArticleCard({ blogs }: Props) {
               className="object-cover"
             />
             {/* タグ */}
-            <div className="absolute  px-3 py-1.5 top-4 right-4 inline-flex gap-1.5 border border-primary-soft  rounded-full w-fit shadow-md  bg-white/95 text-primary items-center">
-              <TagIcon width={12} height={12} />
-              <span className="text-xs font-semibold px-2 py-1">
-                {blog.tags[0]?.name}
-              </span>
-              {blog.tags.length > 1 && (
-                <span className="text-xs font-semibold px-2 py-1 text-primary">
-                  ...
+            {blog.tags.length > 0 && (
+              <div className="absolute  px-3 py-1.5 top-4 right-4 inline-flex gap-1.5 border border-primary-soft  rounded-full w-fit shadow-md  bg-white/95 text-primary items-center">
+                <TagIcon width={12} height={12} />
+                <span className="text-xs font-semibold px-2 py-1">
+                  {blog.tags[0]?.name}
                 </span>
-              )}
-            </div>
+                {blog.tags.length > 1 && (
+                  <span className="text-xs font-semibold px-2 py-1 text-primary">
+                    ...
+                  </span>
+                )}
+              </div>
+            )}
           </div>
           {/* 本文 */}
           <div className="flex flex-col p-6 gap-4">
